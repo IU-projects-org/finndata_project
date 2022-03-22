b988dbd5453e8257fe21cbb3fa67ead253ed7610
AKurmazov <akurmazov2001@gmail.com>
2022-03-22 21:43:24 +0300

    Add WebSocket to fetch live rates' changes

--- lib/repos/stock_info.dart
+++ lib/repos/stock_info.dart
@@ -1,16 +1,21 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
 
+import 'package:dio/dio.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+typedef Handler<String> = void Function(String event);
 const String token = 'c8rdafiad3i8tv0k57n0';
 
 class StockInfoAPIRepository {
   final Dio _dio = Dio();
-  final String _baseUrl = 'https://finnhub.io/api/v1';
+  final String _baseAPIUrl = 'https://finnhub.io/api/v1';
+  final String _baseWSUrl = 'wss://ws.finnhub.io';
   final String _tokenParam = 'token=$token';
 
   Future<Map<String, dynamic>> fetchCompanyProfile(String symbol) async {
     try {
       Response response = await _dio.get(
-        '$_baseUrl/stock/profile2?symbol=$symbol&$_tokenParam'
+        '$_baseAPIUrl/stock/profile2?symbol=$symbol&$_tokenParam'
       );
       return response.data;
     } on Exception catch (exc) {
@@ -21,13 +26,37 @@ class StockInfoAPIRepository {
   Future<Map<String, dynamic>> fetchBasicFinancials(String symbol) async {
     try {
       Response response = await _dio.get(
-        '$_baseUrl/stock/metric?symbol=$symbol&metric=all&$_tokenParam'
+        '$_baseAPIUrl/stock/metric?symbol=$symbol&metric=all&$_tokenParam'
       );
       return response.data;
     } on Exception catch (exc) {
       throw Exception('Not found. Origin: $exc');
     }
   }
+
+  WebSocketChannel initWSChannel(String symbol, Handler<dynamic> handler) {
+    // Init the channel
+    final channel = WebSocketChannel.connect(
+      Uri.parse('$_baseWSUrl?$_tokenParam')
+    );
+
+    // Subscribe to the changes of a particular symbol
+    channel.sink.add(
+      jsonEncode({
+        'type': 'subscribe',
+        'symbol': symbol
+      })
+    );
+
+    // Set up events handler
+    channel.stream.listen(
+      handler,
+      onError: print,
+    );
+
+    return channel;
+  }
+
 }
 
 final StockInfoAPIRepository stockInfoRepo = StockInfoAPIRepository();
--- lib/screens/stock_info.dart
+++ lib/screens/stock_info.dart
@@ -1,8 +1,50 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:finndata_project/repos/stock_info.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class StockChange {
+  final String symbol;
+  final double lastPrice;
+  final int timestamp;
+
+  StockChange(this.symbol, this.lastPrice, this.timestamp);
+
+  StockChange.fromJson(Map<String, dynamic> json)
+    : symbol = json['s'],
+      lastPrice = json['p'],
+      timestamp = json['t'];
+}
+
+class WSResponse {
+  final String type;
+  final List<StockChange> stockChanges;
+
+  WSResponse({required this.type, required this.stockChanges});
+
+  factory WSResponse.fromJson(Map<String, dynamic> json) {
+    final _type = json['type'] as String;
+
+    List<StockChange> _stockChanges = [];
+    if (_type == 'trade') {
+      for (var item in json['data']) {
+        final stockChange = StockChange.fromJson(item as Map<String, dynamic>);
+        _stockChanges.add(stockChange);
+      }
+    }
+
+    return WSResponse(
+      type: _type,
+      stockChanges: _stockChanges
+    );
+  }
+}
 
 class StockInfoScreen extends StatefulWidget {
-  const StockInfoScreen({Key? key, required this.title}) : super(key: key);
+  const StockInfoScreen(
+    {Key? key, required this.title}
+  ) : super(key: key);
 
   final String title;
 
@@ -12,22 +54,47 @@ class StockInfoScreen extends StatefulWidget {
 
 class _StockInfoScreenState extends State<StockInfoScreen> {
 
+  final String symbol = 'AAPL';
   Map<String, dynamic>? _profile;
   Map<String, dynamic>? _financials;
 
+  double _currentRate = 0.0;
+
   void _fetchContent() {
-    stockInfoRepo.fetchCompanyProfile('AAPL')
+    stockInfoRepo.fetchCompanyProfile(symbol)
       .then((response) { setState(() {
         _profile = response;
       }); });
-    stockInfoRepo.fetchBasicFinancials('AAPL')
+    stockInfoRepo.fetchBasicFinancials(symbol)
       .then((response) { setState(() {
         _financials = response;
       }); });
   }
 
+  void _startPollingRates() {
+    void _processWSResponse(dynamic wsResponse) {
+      var parsedResponse = WSResponse.fromJson(jsonDecode(wsResponse));
+      if (parsedResponse.stockChanges.isNotEmpty) {
+        int _latestChangeTimestamp = 0;
+        for (var stockChange in parsedResponse.stockChanges) {
+          if (stockChange.timestamp > _latestChangeTimestamp) {
+            _latestChangeTimestamp = stockChange.timestamp;
+            setState(() {
+              _currentRate = stockChange.lastPrice;
+            });
+          }
+        }
+      }
+    }
+
+    final WebSocketChannel _ = stockInfoRepo.initWSChannel(
+      symbol, _processWSResponse
+    );
+  }
+
   _StockInfoScreenState() {
     _fetchContent();
+    _startPollingRates();
   }
 
   @override
@@ -48,9 +115,17 @@ class _StockInfoScreenState extends State<StockInfoScreen> {
           ),
           body: TabBarView(
             children: [
+              Container(
+                child: Center(
+                  child: Text(
+                    'Current price: $_currentRate\$',
+                    style: const TextStyle(fontSize: 54),
+                  )
+                ),
+                color: Colors.yellow.shade100,
+              ),
               Text('$_profile'),
               Text('$_financials'),
-              Text('3'),
             ]
           ),
         ),
