0584f53215c31da68388f8a8082bd44e9af4ce2d
Tasneem <tasneemsherif23@gmil.com>
2022-05-05 17:18:26 +0300

    useless changes

---
+++ lib/screens/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
